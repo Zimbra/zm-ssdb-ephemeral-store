4b0340dd3a401c72b6fd009ce4f6f70eb32012c5
ilya <raykini@zimbra.com>
2017-06-08 14:19:19 -0700

    deleting set keys one-by-one instead of using flushdb

--- src/java/com/zimbra/qa/unittest/TestSSDBEphemeralStore.java
+++ src/java/com/zimbra/qa/unittest/TestSSDBEphemeralStore.java
@@ -1,5 +1,7 @@
 package com.zimbra.qa.unittest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import junit.framework.TestCase;
@@ -11,6 +13,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.Pair;
 import com.zimbra.cs.account.Config;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.soap.SoapProvisioning;
@@ -36,6 +39,7 @@ public class TestSSDBEphemeralStore extends TestCase {
     private String SAMPLE_AUTH_TOKEN_VERSION = "8.7.0_GA_1659";
     private String ACCOUNT_ID = "47e456be-b00a-465e-a1db-4b53e64fa";
     private boolean SSDBStoreConfigured = false;
+    private List<Pair<EphemeralInput, EphemeralLocation>> toDelete = new ArrayList<Pair<EphemeralInput, EphemeralLocation>>();
 
     @Override
     public void setUp() throws Exception {
@@ -56,19 +60,32 @@ public class TestSSDBEphemeralStore extends TestCase {
     @Override
     public void tearDown() throws Exception {
         if (SSDBStoreConfigured) {
-            String ssdbUrl = Provisioning.getInstance().getConfig().getEphemeralBackendURL();
-            String toks[] = ssdbUrl.split(":");
-            SSDBEphemeralStore.getFactory().shutdown();
-            try {
-                try(JedisPool pool = new JedisPool(toks[1], Integer.parseInt(toks[2]))) {
-                    try (Jedis jedis = pool.getResource()) {
-                        jedis.flushDB();
-                    }
+            for (Pair<EphemeralInput, EphemeralLocation> pair: toDelete) {
+                EphemeralInput input = pair.getFirst();
+                EphemeralLocation location = pair.getSecond();
+                try {
+                    store.delete(input.getEphemeralKey(), (String) input.getValue(), location);
+                } catch (ServiceException e) {
+                    // ignore failed deletions
                 }
-            } catch (ClassCastException e) {
-                //ignore
             }
+            toDelete.clear();
+            SSDBEphemeralStore.getFactory().shutdown();
         }
+    }
+
+    private void addDeletionEntry(EphemeralInput input, EphemeralLocation location) {
+        toDelete.add(new Pair<EphemeralInput, EphemeralLocation>(input, location));
+    }
+
+    private void doSet(EphemeralInput input, EphemeralLocation location) throws ServiceException {
+        store.set(input, location);
+        addDeletionEntry(input, location);
+    }
+
+    private void doUpdate(EphemeralInput input, EphemeralLocation location) throws ServiceException {
+        store.update(input, location);
+        addDeletionEntry(input, location);
     }
 
     @Test
@@ -81,8 +98,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         };
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraLastLogonTimestamp);
         EphemeralInput attr = new EphemeralInput(eKey, firstLogin);
-        store.set(attr, accountIDLocation);
-
+        doSet(attr, accountIDLocation);
         EphemeralResult retAttr = store.get(eKey, accountIDLocation);
         assertNotNull(retAttr);
         assertEquals("Found incorrect last logon timestamp value",firstLogin, retAttr.getValue());
@@ -100,8 +116,8 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraLastLogonTimestamp);
         EphemeralInput attr = new EphemeralInput(eKey, firstLogin);
         EphemeralInput attrLatest = new EphemeralInput(eKey, lastLogin);
-        store.set(attr, accountIDLocation);
-        store.set(attrLatest, accountIDLocation);
+        doSet(attr, accountIDLocation);
+        doSet(attrLatest, accountIDLocation);
         EphemeralResult retAttr = store.get(eKey, accountIDLocation);
         assertNotNull(retAttr);
         assertEquals("Found incorrect last logon timestamp value",lastLogin, retAttr.getValue());
@@ -119,8 +135,8 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraLastLogonTimestamp);
         EphemeralInput attr = new EphemeralInput(eKey, firstLogin);
         EphemeralInput attrLatest = new EphemeralInput(eKey, lastLogin);
-        store.set(attr, accountIDLocation);
-        store.update(attrLatest, accountIDLocation);
+        doSet(attr, accountIDLocation);
+        doUpdate(attrLatest, accountIDLocation);
         EphemeralResult retAttr = store.get(eKey, accountIDLocation);
         assertNotNull(retAttr);
         assertEquals("Found incorrect last logon timestamp value", lastLogin, retAttr.getValue());
@@ -135,7 +151,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         };
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraAuthTokens, SAMPLE_AUTH_TOKEN);
         EphemeralInput attr = new EphemeralInput(eKey, SAMPLE_AUTH_TOKEN_VERSION);
-        store.set(attr, accountIDLocation);
+        doSet(attr, accountIDLocation);
         assertTrue("Should find this auth token", store.has(eKey, accountIDLocation));
     }
 
@@ -149,7 +165,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraAuthTokens, SAMPLE_AUTH_TOKEN);
         EphemeralKey eKey2 = new EphemeralKey(Provisioning.A_zimbraAuthTokens, SAMPLE_AUTH_TOKEN2);
         EphemeralInput attr = new EphemeralInput(eKey, SAMPLE_AUTH_TOKEN_VERSION);
-        store.set(attr, accountIDLocation);
+        doSet(attr, accountIDLocation);
         assertFalse("should not find this auth token ", store.has(eKey2, accountIDLocation));
     }
 
@@ -163,7 +179,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         };
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraCsrfTokenData, SAMPLE_CSRF_TOKEN_CRUMB);
         EphemeralInput attr = new EphemeralInput(eKey, SAMPLE_CSRF_TOKEN_DATA);
-        store.set(attr, accountIDLocation);
+        doSet(attr, accountIDLocation);
         assertTrue("Should find this CSRF token crumb", store.has(eKey, accountIDLocation));
     }
 
@@ -177,7 +193,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey1 = new EphemeralKey(Provisioning.A_zimbraCsrfTokenData, SAMPLE_CSRF_TOKEN_CRUMB);
         EphemeralKey eKey2 = new EphemeralKey(Provisioning.A_zimbraCsrfTokenData, SAMPLE_CSRF_TOKEN_CRUMB2);
         EphemeralInput attr = new EphemeralInput(eKey1, SAMPLE_CSRF_TOKEN_DATA);
-        store.set(attr, accountIDLocation);
+        doSet(attr, accountIDLocation);
         assertFalse("should not find this CSRF token crumb ", store.has(eKey2, accountIDLocation));
     }
 
@@ -191,8 +207,8 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraCsrfTokenData, SAMPLE_CSRF_TOKEN_CRUMB);
         EphemeralInput attr = new EphemeralInput(eKey, SAMPLE_CSRF_TOKEN_DATA);
         EphemeralInput updatedAttr = new EphemeralInput(eKey, SAMPLE_CSRF_TOKEN_DATA2);
-        store.set(attr, accountIDLocation);
-        store.set(updatedAttr, accountIDLocation);
+        doSet(attr, accountIDLocation);
+        doSet(updatedAttr, accountIDLocation);
         assertTrue("Should find the CSRF token crumb that was just saved", store.has(eKey, accountIDLocation));
         assertEquals(SAMPLE_CSRF_TOKEN_DATA2, store.get(eKey, accountIDLocation).getValue());
     }
@@ -212,9 +228,9 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralInput attrVal2 = new EphemeralInput(eKey2, SAMPLE_CSRF_TOKEN_DATA2);
         EphemeralInput attrVal3 = new EphemeralInput(eKey3, SAMPLE_CSRF_TOKEN_DATA3);
 
-        store.set(attrVal1, accountIDLocation);
-        store.set(attrVal2, accountIDLocation);
-        store.set(attrVal3, accountIDLocation);
+        doSet(attrVal1, accountIDLocation);
+        doSet(attrVal2, accountIDLocation);
+        doSet(attrVal3, accountIDLocation);
 
         assertTrue(store.has(eKey1, accountIDLocation));
         assertTrue(store.has(eKey2, accountIDLocation));
@@ -240,9 +256,9 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralInput attrVal2 = new EphemeralInput(eKey2, SAMPLE_AUTH_TOKEN_VERSION);
         EphemeralInput attrVal3 = new EphemeralInput(eKey3, SAMPLE_AUTH_TOKEN_VERSION);
 
-        store.set(attrVal1, accountIDLocation);
-        store.set(attrVal2, accountIDLocation);
-        store.set(attrVal3, accountIDLocation);
+        doSet(attrVal1, accountIDLocation);
+        doSet(attrVal2, accountIDLocation);
+        doSet(attrVal3, accountIDLocation);
 
         assertTrue(store.has(eKey1, accountIDLocation));
         assertTrue(store.has(eKey2, accountIDLocation));
@@ -263,7 +279,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey1 = new EphemeralKey(Provisioning.A_zimbraAuthTokens, SAMPLE_AUTH_TOKEN);
         RelativeExpiration exp = new RelativeExpiration(1000L, TimeUnit.MILLISECONDS);
         EphemeralInput attrVal1 = new EphemeralInput(eKey1, SAMPLE_AUTH_TOKEN_VERSION, exp);
-        store.set(attrVal1, accountIDLocation);
+        doSet(attrVal1, accountIDLocation);
         assertTrue("Token should be in SSDB before it expires", store.has(eKey1, accountIDLocation));
         Thread.sleep(2000);
         assertFalse("Token should be gone after 2 seconds", store.has(eKey1, accountIDLocation));
@@ -279,7 +295,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         EphemeralKey eKey1 = new EphemeralKey(Provisioning.A_zimbraAuthTokens, SAMPLE_AUTH_TOKEN);
         RelativeExpiration exp = new RelativeExpiration(600000L, TimeUnit.MILLISECONDS);
         EphemeralInput attrVal1 = new EphemeralInput(eKey1, SAMPLE_AUTH_TOKEN_VERSION, exp);
-        store.set(attrVal1, accountIDLocation);
+        doSet(attrVal1, accountIDLocation);
         assertTrue("Token should be in SSDB after insertion", store.has(eKey1, accountIDLocation));
         store.delete(eKey1, SAMPLE_AUTH_TOKEN_VERSION, accountIDLocation);
         assertFalse("Token should be gone from SSDB after deletion", store.has(eKey1, accountIDLocation));
@@ -295,7 +311,7 @@ public class TestSSDBEphemeralStore extends TestCase {
         };
         EphemeralKey eKey = new EphemeralKey(Provisioning.A_zimbraLastLogonTimestamp);
         EphemeralInput attr = new EphemeralInput(eKey, lastLogon);
-        store.set(attr, accountIDLocation);
+        doSet(attr, accountIDLocation);
         assertTrue("Last logon value should be present before deletion", store.has(eKey, accountIDLocation));
         store.delete(eKey, lastLogon, accountIDLocation);
         assertFalse("Last logon value should be gone after deletion", store.has(eKey, accountIDLocation));
